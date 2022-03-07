a43bbe9e87d99de0e8866d5d76c3c39444125918
Steven Schmidt <stevenschmidtaustex@gmail.com>
2022-03-06 20:54:53 -0600

    Product routes added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findAll();
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag, through: ProductTag}]
+    });
+
+    if (!prodData) {
+      res.status(404).json({ message: 'No product found with this id.' });
+      return;
+    }
+
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -91,6 +107,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const prodData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!prodData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
